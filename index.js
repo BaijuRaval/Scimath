936e726a3609521a7ccb42faf1a1ccf1950797e1
BaijuRaval <baijuraval2012@gmail.com>
2025-05-01 14:54:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.post('/verify-payment', (req, res) => {
 
   const generatedSignature = crypto
     .createHmac('sha256', process.env.RAZORPAY_KEY_SECRET)
-    .update(  .update(razorpay_order_id + '|' + razorpay_payment_id)
+    .update(razorpay_order_id + '|' + razorpay_payment_id)
     .digest('hex');
 
   if (generatedSignature === razorpay_signature) {
